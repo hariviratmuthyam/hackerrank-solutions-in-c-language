5150cb3e5f1ca72f76ccae8a4b066fde5b9b8348
Harivirat Muthyam <56294560+hariviratmuthyam@users.noreply.github.com>
2021-05-18 18:11:12 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -61,6 +61,7 @@ int main()
     for(int i=0;i<len;i++){
         for(int j=0;j<len;j++){
             int min = i < j ? i : j;
+		//if i<j is true then min = i else min =j
             min = min < len-i ? min : len-i-1;
             min = min < len-j-1 ? min : len-j-1;
             printf("%d ", n-min);
