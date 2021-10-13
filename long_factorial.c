91cf82819bc0e306bb1c07aa7c0b582dd96ec35b
Harivirat Muthyam <56294560+hariviratmuthyam@users.noreply.github.com>
2021-10-13 11:24:32 +0530

    Create long_factorial.c

---
+++ long_factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,i;
+    long int f=1;
+    scanf("%d", &n);
+    for(i=n;i>0;i--){
+        f=f*i;
+    
+    }
+    printf("%ld", f);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
