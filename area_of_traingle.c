f0d88e56fc1ba9262da8ffb130bb2dd9d091573c
Harivirat Muthyam <56294560+hariviratmuthyam@users.noreply.github.com>
2021-10-13 11:14:53 +0530

    Create area_of_traingle.c

---
+++ area_of_traingle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int l;
+    long int b, a;
+    scanf("%d%ld",&l,&b);
+    a=l*b;
+    printf("%ld",a);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
